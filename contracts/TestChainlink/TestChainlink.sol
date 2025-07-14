572f4ba8918852926099e5e8ad1e944d08edcca0
sukanyaparashar <sukanyaparashar111@gmail.com>
2025-07-14 16:01:17 +0530

    Add Chainlink price feeds example

---
+++ contracts/TestChainlink/TestChainlink.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract TestChainlink {
+    /**
+     * Returns the latest price for specific price feed
+     */
+    function getLatestPrice(address _priceFeed) external view returns (int) {
+        (
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = AggregatorV3Interface(_priceFeed).latestRoundData();
+        return price;
+    }
+
+    /**
+     * Returns the decimals for specific price feed
+     */
+    function getDecimals(address _priceFeed) external view returns (uint8) {
+        return AggregatorV3Interface(_priceFeed).decimals();
+    }
+}
